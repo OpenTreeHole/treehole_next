c3c648399bc44e7e69aa56b566a3d7f7b69287de
顶碗人 <jsclndnz@outlook.com>
2022-06-06 10:42:40 +0800

    style: singular table name

--- db/init.go
+++ db/init.go
@@ -4,17 +4,23 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 	"treehole_next/config"
 )
 
 var DB *gorm.DB
+var gormConfig = &gorm.Config{
+	NamingStrategy: schema.NamingStrategy{
+		SingularTable: true, // use singular table name, table for `User` would be `user` with this option enabled
+	},
+}
 
 func InitDB() {
 	var err error
 	if config.Config.Debug {
-		DB, err = gorm.Open(sqlite.Open("db/sqlite.db"), &gorm.Config{})
+		DB, err = gorm.Open(sqlite.Open("db/sqlite.db"), gormConfig)
 	} else {
-		DB, err = gorm.Open(mysql.Open(config.Config.DbUrl), &gorm.Config{})
+		DB, err = gorm.Open(mysql.Open(config.Config.DbUrl), gormConfig)
 	}
 	if err != nil {
 		panic(err)
