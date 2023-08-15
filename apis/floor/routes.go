193642a31159e5ac79c793ced0712a1eaa11758f
ZhuJunjie <zhujunjie.4396@bytedance.com>
2023-08-15 19:27:34 +0800

    fix: only posting holes and floors use AnswerQuestion Middleware

--- apis/floor/routes.go
+++ apis/floor/routes.go
@@ -1,13 +1,16 @@
 package floor
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"treehole_next/bootstrap"
+)
 
 func RegisterRoutes(app fiber.Router) {
 	app.Get("/holes/:id/floors", ListFloorsInAHole)
 	app.Get("/floors", ListFloorsOld)
 	app.Get("/floors/:id", GetFloor)
-	app.Post("/holes/:id/floors", CreateFloor)
-	app.Post("/floors", CreateFloorOld)
+	app.Post("/holes/:id/floors", bootstrap.MiddlewareHasAnsweredQuestions, CreateFloor)
+	app.Post("/floors", bootstrap.MiddlewareHasAnsweredQuestions, CreateFloorOld)
 	app.Put("/floors/:id", ModifyFloor)
 	app.Post("/floors/:id/like/:like", ModifyFloorLike)
 	app.Delete("/floors/:id", DeleteFloor)
--- apis/hole/routes.go
+++ apis/hole/routes.go
@@ -1,6 +1,9 @@
 package hole
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"treehole_next/bootstrap"
+)
 
 func RegisterRoutes(app fiber.Router) {
 	app.Get("/divisions/:id/holes", ListHolesByDivision)
@@ -8,8 +11,8 @@ func RegisterRoutes(app fiber.Router) {
 	app.Get("/users/me/holes", ListHolesByMe)
 	app.Get("/holes/:id", GetHole)
 	app.Get("/holes", ListHolesOld)
-	app.Post("/divisions/:id/holes", CreateHole)
-	app.Post("/holes", CreateHoleOld)
+	app.Post("/divisions/:id/holes", bootstrap.MiddlewareHasAnsweredQuestions, CreateHole)
+	app.Post("/holes", bootstrap.MiddlewareHasAnsweredQuestions, CreateHoleOld)
 	app.Patch("/holes/:id", PatchHole)
 	app.Put("/holes/:id", ModifyHole)
 	app.Delete("/holes/:id", HideHole)
