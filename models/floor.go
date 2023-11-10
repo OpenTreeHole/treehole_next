eee184ae5834f3fc7fe5a3eb785e9b40c5f7f6e9
Ke Chen <JingYiJun3104@outlook.com>
2023-11-10 23:39:32 +0800

    feat: set fold for sensitive floor

--- models/floor.go
+++ models/floor.go
@@ -173,6 +173,7 @@ func (floor *Floor) SetDefaults() {
 
 	if floor.IsSensitive == true && (floor.IsActualSensitive == nil || *floor.IsActualSensitive == true) {
 		floor.Content = "该内容被猫猫吃掉了"
+		floor.FoldFrontend = []string{"该内容被猫猫吃掉了"}
 	}
 }
 
