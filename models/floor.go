450be7c9ec766e5abb1692b75b19fcf171146c5f
PinappleUnderTheSea <1411493875@qq.com>
2024-04-03 20:11:14 +0800

    fix: set deleted content before sensitive content

--- models/floor.go
+++ models/floor.go
@@ -183,7 +183,10 @@ func (floor *Floor) SetDefaults(c *fiber.Ctx) (err error) {
 	}
 
 	floor.Anonyname = utils.GetFuzzName(floor.Anonyname)
-	if floor.Sensitive() {
+	if floor.Deleted {
+		floor.Content = "该内容因违反社区公约被删除"
+		floor.Anonyname = ""
+	} else if floor.Sensitive() {
 		if !user.IsAdmin {
 			if floor.UserID == user.ID {
 				floor.Content = "该内容被猫猫吃掉了"
