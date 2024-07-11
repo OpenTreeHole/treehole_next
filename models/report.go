8128df7eaa7ac98adc5536de914712aa8d2d6ea8
polaris <2673986789@qq.com>
2024-07-11 17:07:50 +0800

    fix: #154

--- models/report.go
+++ models/report.go
@@ -101,7 +101,10 @@ func (report *Report) Create(c *fiber.Ctx, db ...*gorm.DB) error {
 		}
 	} else {
 		existingReport.Reason = existingReport.Reason + "\n" + report.Reason
-		err = tx.Model(&existingReport).Updates(Report{Reason: existingReport.Reason, Dealt: false}).Error // update reason and load floor in AfterUpdate hook
+		err = tx.Model(&existingReport).Updates(map[string]interface{}{
+			"reason": existingReport.Reason,
+			"dealt":  false,
+		}).Error // update reason and load floor in AfterUpdate hook
 		if err != nil {
 			return err
 		}
