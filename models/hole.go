69cc6c0cab9f6a1b4faef3dba2c1f3542241626a
ppolariss <2673986789@qq.com>
2026-02-14 19:43:35 +0800

    fix: set frozenFrontend default false

--- models/hole.go
+++ models/hole.go
@@ -75,7 +75,8 @@ type Hole struct {
 	HoleID int `json:"hole_id" gorm:"-:all"`
 
 	// 冻结状态，仅管理员可见 true
-	FrozenFrontend *bool `json:"frozen,omitempty" gorm:"-:all"`
+	// FrozenFrontend *bool `json:"frozen,omitempty" gorm:"-:all"`
+	FrozenFrontend *bool `json:"frozen" gorm:"-:all"`
 
 	// 返回给前端的楼层列表，包括首楼、尾楼和预加载的前 n 个楼层
 	HoleFloor struct {
@@ -324,6 +325,7 @@ func (holes Holes) Preprocess(c *fiber.Ctx) error {
 	user, err := GetCurrLoginUser(c)
 	if err == nil {
 		for _, hole := range holes {
+			// compatibility for swift client
 			frozenFrontend := user.IsAdmin && hole.Frozen
 			hole.FrozenFrontend = &frozenFrontend
 		}
